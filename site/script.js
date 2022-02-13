6d639939354453cbbea8bb8098c347b4018e454c
Caique De Brito <caiquedebritoo@gmail.com>
2022-02-13 13:36:05 -0300

    Alteração na animação das imagens

--- site/script.js
+++ site/script.js
@@ -1,14 +1,19 @@
 let imgs = document.querySelectorAll('[data-anima]')
 // const img1 = document.getElementsByClassName('img1')
-// console.log(imgs[0].getBoundingClientRect().y)
 function animaScroll() {
-    // Pega as posições das images no eixo y
-    let img1Y = imgs[0].getBoundingClientRect().y
-    if (img1Y < 350) {
-        for (let img of imgs) {
+    let top = window.scrollY 
+    imgs.forEach(img => {
+        if (top > img.offsetTop) {
             img.classList.add('animacao')
         }
-    }
+    })
+    // Pega as posições das images no eixo y
+    // let img1Y = imgs[0].getBoundingClientRect().y
+    // if (img1Y < 350) {
+    //     for (let img of imgs) {
+    //         img.classList.add('animacao')
+    //     }
+    // }
 
 }
 
